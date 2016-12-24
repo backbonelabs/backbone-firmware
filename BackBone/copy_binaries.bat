d0b2169b6768e76b9b370fa6128ee40e278a10fc
Kris Henderson <kris.henderson76@gmail.com>
2016-12-24 10:50:35 -0800

    Add batch file to copy binaries

---
+++ BackBone/copy_binaries.bat
@@ -0,0 +1,5 @@
+REM Run from the same directory this file is in
+
+copy BackBone_Bootloadable.cydsn\CortexM0\ARM_GCC_493\Debug\BackBone_Bootloadable.cyacd ..\Binaries
+copy BackBone_Bootloadable.cydsn\CortexM0\ARM_GCC_493\Debug\BackBone_Bootloadable.hex ..\Binaries
+copy stack.cydsn\CortexM0\ARM_GCC_493\Debug\stack.cyacd ..\Binaries
