285e285ac553f530259d970e293ab1eb36e8ff70
Kris Henderson <kris.henderson76@gmail.com>
2016-12-24 10:51:09 -0800

    Zero duration sessions last forever
      - or until the battery dies
      - also incremented the version number

--- BackBone/BackBone_Bootloadable.cydsn/version.h
+++ BackBone/BackBone_Bootloadable.cydsn/version.h
@@ -16,6 +16,6 @@
 #define HW_MAJOR_VERSION            (1u)
 #define HW_MINOR_VERSION            (0u)
 #define FW_MAJOR_VERSION            (1u)
-#define FW_MINOR_VERSION            (11u)
+#define FW_MINOR_VERSION            (12u)
 
 #endif /* VERSION_H_*/
