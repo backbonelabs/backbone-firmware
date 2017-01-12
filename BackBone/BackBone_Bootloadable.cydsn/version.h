aa008b2eeaed1455accaacc17b330f19c08005fb
Kris Henderson <kris.henderson76@gmail.com>
2017-01-12 16:26:28 +0700

    Release build of firmware 1.17
      - Updated project configuration so the Release build
        uses the correct linker script.

--- BackBone/BackBone_Bootloadable.cydsn/version.h
+++ BackBone/BackBone_Bootloadable.cydsn/version.h
@@ -16,6 +16,6 @@
 #define HW_MAJOR_VERSION            (1u)
 #define HW_MINOR_VERSION            (0u)
 #define FW_MAJOR_VERSION            (1u)
-#define FW_MINOR_VERSION            (16u)
+#define FW_MINOR_VERSION            (17u)
 
 #endif /* VERSION_H_*/
