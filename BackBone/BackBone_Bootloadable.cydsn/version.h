2a1dda6dcf03b6df0c19ef473da85d1a7d1f4fbb
Kris Henderson <kris.henderson76@gmail.com>
2017-02-18 02:16:59 +0700

    Firmware Version 1.22 binaries

--- BackBone/BackBone_Bootloadable.cydsn/version.h
+++ BackBone/BackBone_Bootloadable.cydsn/version.h
@@ -16,6 +16,6 @@
 #define HW_MAJOR_VERSION            (1u)
 #define HW_MINOR_VERSION            (0u)
 #define FW_MAJOR_VERSION            (1u)
-#define FW_MINOR_VERSION            (21u)
+#define FW_MINOR_VERSION            (22u)
 
 #endif /* VERSION_H_*/
